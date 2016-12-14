953e23a9b6efcb1f7d19bf581684ad8f8ec85b23
Jason Mitchell <jason@affirm.com>
2016-12-14 00:11:29 -0800

    Copyright Bump

--- files-xml-read.cpp
+++ files-xml-read.cpp
@@ -9,7 +9,7 @@
  * Permission to use, modify and distribute is granted via the
  *  GNU Affero General Public License v3 or later
  *
- *  Copyright (C) 2007-2012 Jason Mitchell, Randi Mitchell
+ *  Copyright (C) 2007-2016 Jason Mitchell, Randi Mitchell
  *     Contributions by Tim Callahan, Jonathan Hseu
  *  Based on Mordor (C) Brooke Paul, Brett J. Vickers, John P. Freeman
  *
