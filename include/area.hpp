b7363ec14f5d8f6fdc4f401ce89f5587efc329dc
Jason Mitchell <mitchej@gmail.com>
2021-11-07 20:15:40 -0800

    Copyright Bump

--- include/area.hpp
+++ include/area.hpp
@@ -10,7 +10,7 @@
  * Permission to use, modify and distribute is granted via the
  *  GNU Affero General Public License v3 or later
 
- *  Copyright (C) 2007-2020 Jason Mitchell, Randi Mitchell
+ *  Copyright (C) 2007-2021 Jason Mitchell, Randi Mitchell
  *     Contributions by Tim Callahan, Jonathan Hseu
  *  Based on Mordor (C) Brooke Paul, Brett J. Vickers, John P. Freeman
  *
