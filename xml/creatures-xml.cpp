f14e0aec0d572179eb6e783cfe3cf26fea736123
ocelot134269 <tim.callahan2009@gmail.com>
2022-11-09 23:38:39 -0600

    update linothan paladins
    Bumped version to 2.56c
    
    Set existing Linothan paladins so they receive "hands" skill if level 13+; Initial skill level = level*9

--- xml/creatures-xml.cpp
+++ xml/creatures-xml.cpp
@@ -414,8 +414,13 @@ int Creature::readFromXml(xmlNodePtr rootNode, bool offline) {
                 else
                     daily[DL_TELEP].cur = 3;
                 if (getClass() == CreatureClass::MAGE || getClass() == CreatureClass::LICH)
-                    daily[DL_TELEP].cur = std::min(10, (int)getSkillLevel("translocation")/5);
+                    daily[DL_TELEP].cur = std::max(1,std::min(10, (int)getSkillLevel("translocation")/5));
 
+             }
+             if(getVersion() < "2.56c") {
+                if (getClass() == CreatureClass::PALADIN && deity == LINOTHAN && level >=13) {
+                    addSkill("hands",(level*9));
+                }
              }
         }
     }
