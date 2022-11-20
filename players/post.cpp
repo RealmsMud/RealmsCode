a975630e26cb343944811c5a6baadc4cf1cbdd41
ocelot134269 <tim.callahan2009@gmail.com>
2022-11-20 03:03:12 -0600

    Update post.cpp
    use isPksafe() rather than checking R_SAFE_ROOM....

--- players/post.cpp
+++ players/post.cpp
@@ -57,7 +57,7 @@ bool canPost(std::shared_ptr<Player> player) {
     if(!player->isStaff()) {
         if( !player->getRoomParent()->flagIsSet(R_POST_OFFICE) &&
             !player->getRoomParent()->flagIsSet(R_LIMBO) &&
-            !(player->getRoomParent()->flagIsSet(R_FAST_HEAL) && player->getRoomParent()->flagIsSet(R_SAFE_ROOM))
+            !(player->getRoomParent()->flagIsSet(R_FAST_HEAL) && player->getRoomParent()->isPkSafe())
         ) {
             *player << "You cannot do that here.\nYou need to find a post office, or you need to be in a tick room that is pkill safe.\n";
             return(false);
