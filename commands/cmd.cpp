8a138748ca48f4d14489c042c59767face5e417a
ocelot134269 <tim.callahan2009@gmail.com>
2023-07-14 07:29:27 -0500

    -Fixed new mudmail message on login -Added unread mudmail notification every 10 min -Auto-stand for everywhere it refused to do something when you were sitting. Except for steal and sneak (since standing unhides) -New spell shortcuts

--- commands/cmd.cpp
+++ commands/cmd.cpp
@@ -554,6 +554,7 @@ bool Config::initCommands() {
     playerCommands.emplace("colours", 100, cmdColors, nullptr, "Show colors and set custom colors");
     playerCommands.emplace("factions", 100, cmdFactions, nullptr, "Show your standing with various factions");
     playerCommands.emplace("recipes", 100, cmdRecipes, nullptr, "Show what recipes you know");
+    playerCommands.emplace("gamble", 100, cmdGamble, nullptr, "Gamble in a casino.");
 
     // player-only communication functions
     playerCommands.emplace("send", 100, communicateWith, nullptr, "Send a message to another player");
